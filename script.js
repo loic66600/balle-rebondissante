635396ec97f202bc9f4cc613fd26f47d689e735e
loic <loic.rossignol66@gmail.com>
2024-04-09 15:58:21 +0200

    ballreb

---
+++ script.js
@@ -0,0 +1,9 @@
+//configurer le canvas
+//le canvas est une zone de dessin qui permet de dessiner des formes 2d ou 3d
+
+//on selection l element canvas dans le dom
+const canvas = document.querySelector('canvas');
+
+//on definit le contexte 2d ou 3d de canvas
+let ctx = canvas.getContext('2d');
+
